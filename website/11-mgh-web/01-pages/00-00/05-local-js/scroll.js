27c4505cb89659cf48496ec4f4e843b0bb34c08a
mgledhill <mg@practicalseries.com>
2025-12-30 15:20:18 +0000

    Cnonical update via ChatGPT

--- website/11-mgh-web/01-pages/00-00/05-local-js/scroll.js
+++ website/11-mgh-web/01-pages/00-00/05-local-js/scroll.js
@@ -1,1 +1,114 @@
-$(document).ready(function(){$(".aside-right-rev").append("<p>scroll.js&nbsp;&mdash;&nbsp;R01</p>"),$(".js--sc-000000").click(function(){$("html, body").animate({scrollTop:$("#js--000000").offset().top-80},1e3)})});
+$(document).ready(function(){$(".aside-right-rev").append("<p>scroll.js&nbsp;&mdash;&nbsp;R01</p>"),$(".js--sc-000000").click(function(){$("html, body").animate({scrollTop:$("#js--000000").offset().top-80},1e3)})});/* PRACTICALSERIES (c) 2016
+
+*******************************************************************************
+Title :          LOCAL SCROLL - JQUERY                          98-00-SCROLL.JS
+*******************************************************************************
+
+PRACTICALSERIES: Practical Series of Publications by Michael Gledhill
+                 Published in the United Kingdom
+
+                 Email: mg@practicalseries.com
+                 Web:   https://practicalseries.com
+
+-------------------------------------------------------------------------------
+DETAILS         LOCAL SCROLL FUNCTIONS
+
+jQuery is using the latest version from the GoogleAPIs library.
+
+This script applies the scroll to a point within the page (scrollTop)
+functionality for the current page.
+
+-------------------------------------------------------------------------------
+NAMING CONVENTIONS
+
+Each <section> of the site has a waypoint (WP) reference number that identifies
+the start of each section. These are id attributes and have the format:
+
+        #js--ccssii
+
+    where:  cc - chapter number
+            ss - subsection number
+            ii - inline section number
+
+The six digit number is unique to each section, i.e. section 2.1.3
+would have waypoint reference js--020103
+
+The trigger class used in the <a> element to trigger the scroll to the waypoint
+has the name .js-sc-000000 where the six digit number matches the waypoint
+number of the section that is to be scrolled to.
+
+The scrollTop function has an offset of -60px to ensure the title is visible
+below the fixed navigation bar.
+
+-------------------------------------------------------------------------------
+MODIFICATION HISTORY:
+
+This is a complete summary of all software modifications.
+
+Date          Issue        Author         Reason for Modification
+-------------------------------------------------------------------------------
+09 May 2019   R01          M. Gledhill    First release (all files updated)
+
+27 Mar 2019   P14          M. Gledhill    Published
+
+20 Mar 2019   d-P13.00.03  M. Gledhill    Sections updated
+
+31 May 2017   P10          M. Gledhill    Migration to Git VCS
+                                          FIRST PUBLISHED COMMIT
+                                          Based on PS(LIVE-non VCS) P06.02
+
+                                          - .aside-revision class
+                                            name change (.aside-right-rev)
+---------------------------------------------------------------------------- */
+
+
+/* ****************************************************************************
+   WAIT FOR PAGE TO LOAD
+   ****************************************************************************
+   No jQuery actions take place until the webpage has loaded, all JQuery code
+   is contained within the .ready(function()).
+   ************************************************************************* */
+
+
+$(document).ready(function() {                  /* START OF PAGE READY FUNCTION */
+
+/* ****************************************************************************
+   REVISION
+   **************************************************************************** */
+
+    $('.aside-right-rev').append (
+        "<p>scroll.js&nbsp;&mdash;&nbsp;R01</p>" /* LOCAL JS REVISION NUMBER */
+    );
+
+
+
+/* ****************************************************************************
+   SCROLL TO SECTION
+   ****************************************************************************
+   This holds several scrollTop functions that scroll from the TOC to the
+   specified section. There is one click function per <a> within the TOC.
+   ************************************************************************* */
+
+
+/* ---------------------------------------------------------------------------
+   SCROLL TO TOP
+   The offset is set to .to -80 - e.g.
+
+    $('.js--sc-000000').click(function () {
+       $('html, body').animate({scrollTop: $('#js--000000').offset().top -80}, 1000);
+    });
+   ------------------------------------------------------------------------- 
+   $('.js--sc-000000').click(function () {
+       $('html, body').animate({scrollTop: $('#js--000000').offset().top -80}, 1000);
+    }); */
+
+   $('.js--sc-000000').on('click', function (e) {
+      e.preventDefault(); // stops the instant jump
+      $('html, body').stop().animate(
+         { scrollTop: $('#js--000000').offset().top - 80 },
+         1000
+      );
+   });
+
+
+});                                             /* END OF PAGE READY FUNCTION */
